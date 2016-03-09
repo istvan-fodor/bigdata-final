5ccbe05d42f4eb8e83e3c5bf3ba84f61165b718c
Istvan Fodor <Istvan_Fodor@bcbsil.com>
2016-03-09 17:26:06 -0600

    changed factors

--- transform.R
+++ transform.R
@@ -5,18 +5,21 @@ fileName <- 'read_all.sql'
 query <- readChar(fileName, file.info(fileName)$size)
 
 college <- dbGetQuery(con, query)
-college$region <- as.factor(college$region)
-college$control <- as.factor(college$control)
-college$locale <- as.factor(college$locale)
-college$carnegie <- as.factor(college$carnegie)
-college$religious_affil <- as.factor(college$religious_affil)
-college$predominant_degree <- as.factor(college$predominant_degree)
-college$highest_degree <- as.factor(college$highest_degree)
-college$logsize <- log(college$size)
-college$log_tuition_in_state <- log(college$tuition_in_state) 
+college$region <- factor(college$region, levels = c(NA, levels(college$region)))
+college$control <- factor(college$control, levels = c(NA, levels(college$control)))
+college$locale <- factor(college$locale, levels = c(NA, levels(college$locale)))
+college$carnegie <- factor(college$carnegie, levels = c(NA, levels(college$carnegie)))
+college$religious_affil <- factor(college$religious_affil, levels = c(NA, levels(college$religious_affil)))
+college$predominant_degree <- factor(college$predominant_degree, levels = c(NA, levels(college$predominant_degree)))
+college$highest_degree <- factor(college$highest_degree, levels = c(NA, levels(college$highest_degree)))
+college$logsize <- log(1+college$size)
+college$log_tuition_in_state <- log(1+college$tuition_in_state) 
+
+
+for (i in 1:ncol(college)) {
+  if (class(college[,i]) == "integer") {
+    college[,i] <- as.numeric(college[,i])
+  }
+}
+
 college_pred <- college[,-1]
-
-sum <- summary(college)
-
-fit <- glm( ten_yrs_after_entry_median ~ ., data = college_pred, na.action = "na.exclude")
-
