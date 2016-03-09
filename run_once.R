4397d75dc305a9ed1df16beac5ee17c0e36d9f40
Istvan Fodor <Istvan_Fodor@bcbsil.com>
2016-03-09 15:29:48 -0600

    Added run_once.R

--- run_once.R
+++ run_once.R
@@ -1,7 +1,7 @@
 library(RSQLite)
 con <- dbConnect(SQLite(), dbname="database.sqlite")
 
-fileName <- 'read_all.sql'
+fileName <- 'run_once.sql'
 query <- readChar(fileName, file.info(fileName)$size)
 results <- dbGetQuery(con, query)
 
